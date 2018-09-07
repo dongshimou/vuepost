c97d3bf002c46fb408dc444a910c1f6857aba51d
skadi <dongshimou@smm.cn>
2018-09-07 17:45:46 +0800

    add create

--- src/components/single_ss.vue
+++ src/components/single_ss.vue
@@ -1,6 +1,7 @@
 <template>
-<div class="flexColBox">
-    <div id="server" class="flexColBox" :class="server.is_online ? online:offline">
+<div class="flexRowBox flexRowBoxLeft server-container">
+    <div id="server" class="flexRowBox" :class="server.is_online ? online:offline">
+      <div class="flexRowBox channels-container">
         <div id="channels">
             <v-popover
             offset="16"
@@ -20,70 +21,153 @@
             </v-popover>
 
         </div>
+      </div>
+    </div>
+
+    <div class="channels-add flexColBox">
+        <button @click="addServer" :disabled='!server.is_online'>
+          生成
+        </button>
     </div>
 </div>
 </template>
 
 <script>
+import Global from "@/components/global";
+
 export default {
+  components: {
+    Global
+  },
   data() {
     return {
+      server: {},
       offline: "Offline",
       online: "Online",
+      enable: "",
+      disable: "Disable",
       channelStatus: [],
       chan: {}
     };
   },
   props: {
-    server: Object
+    host: String
   },
   methods: {
     getChan: function(cc) {
-      if(!cc){
-        return
+      if (cc.chan.length == 0) {
+        return;
       }
-      // console.log(cc.chan);
-        let index = Math.floor(Math.random() * cc.chan.length);
-    //   console.log(index);
-    //   console.log(cc.chan[index]);
-      this.chan=cc.chan[index]
-      console.log(this.chan)
+      let index = Math.floor(Math.random() * cc.chan.length);
+      this.chan = cc.chan[index];
+      // console.log(this.chan)
     },
+    addServer: function() {
+      let address = Global.ss1 + "/admin/ss/add";
+      this.$http
+        .get(address, {
+          params: {
+            host: this.host
+          }
+        })
+        .then(
+          res => {
+            if (res.body.code == 1000) {
+              let data = res.body.data;
+              let temp = {
+                host: data.host,
+                port: data.port,
+                method: data.method,
+                password: data.password,
+                flow_count: data.flow_count,
+                dead_time: data.dead_time
+              };
+              console.log(this.server.channels);
+              this.server.channels.push(temp);
+              this.initServer();
+              console.log("success");
+              this.$noty.success("["+this.host+"]:生成成功");
+            } else {
+              this.$noty.error(res.body.msg);
+            }
+          },
+          res => {
+            this.$noty.error(res.statusText);
+          }
+        );
+    },
+    fetchServer: function() {
+      // console.log(this.host)
+      let address = Global.ss1 + "/admin/server/query";
+      this.$http
+        .get(address, {
+          params: {
+            channels: 1,
+            host: this.host
+          }
+        })
+        .then(
+          res => {
+            if (res.body.code == 1000) {
+              let data = res.body.data;
+              // console.log(data.list)
+              if (data.list == null) {
+                this.$noty.error("server : [" + this.host + "] is null");
+                return;
+              }
+              if (data.list.length != 1) {
+                this.$noty.error("Get Server Error");
+                return;
+              }
+              this.server = data.list[0];
+              this.initServer();
+            }
+          },
+          res => {
+            // console.log(res.statusText)
+            this.$noty.error(res.statusText);
+          }
+        );
+    },
+    initServer: function() {
+      let channels = this.server.channels;
+      let max = this.server.max;
+      let host = this.server.host;
+      let method = this.server.method;
+      this.channelStatus = [];
+
+      
+      let clen = channels.length;
+      let scale = 10.0 / max;
+      let Fscale = max / 10.0;
+      let sl = Math.ceil(scale * clen);
+      for (let i = 0; i < 10; i++) {
+        let temp = {
+          status: "",
+          id: i,
+          chan: []
+        };
+        if (i < sl) {
+          temp.status = "exist";
+        } else {
+          temp.status = "not";
+        }
+        let l = i * Fscale;
+        let r = l + Fscale;
+        for (let j = l; j < r; j++) {
+          if (channels[j]) {
+            let tc = channels[j];
+            tc["host"] = host;
+            tc["method"] = method;
+            temp.chan.push(tc);
+          }
+        }
+        this.channelStatus.push(temp);
+      }
+    }
   },
   mounted() {
-    let channels = this.server.channels;
-    let max = this.server.max;
-    let host = this.server.host;
-    let method = this.server.method;
-
-    let len = channels.length;
-    let scale = 10.0 / max;
-    let Fscale = max / 10.0;
-    let sl = Math.ceil(scale * len);
-    console.log(scale, sl);
-    for (let i = 0; i < 10; i++) {
-      let temp = {
-        status: "",
-        id: i,
-        chan: []
-      };
-      if (i < sl) {
-        temp.status = "exist";
-      } else {
-        temp.status = "not";
-      }
-      let l = i * Fscale;
-      let r = l + Fscale;
-      for (let j = l; j < r; j++) {
-        if (channels[j]) {
-          let tc = channels[j];
-          tc["host"] = host;
-          tc["method"] = method;
-          temp.chan.push(tc);
-        }
-      }
-      this.channelStatus.push(temp);
-    }
+    this.fetchServer();
   }
 };
 </script>
@@ -112,13 +196,50 @@ export default {
 $acolor: rgba(0, 200, 0, 0.4);
 $dcolor: rgba(200, 0, 0, 0.4);
 $more: rgba(0, 0, 0, 0.5);
+$height: 100px;
+$serverWidth: 200px;
+$buttonWidth: 100px;
+
+$aDark:rgba(160, 160, 160, 0.3);
+$dDark:rgba(160, 160, 160, 0.8);
+$myLove:#66ccff;
+.server-container {
+  width: $serverWidth + $buttonWidth;
+}
+.channels-container {
+  width: 100%;
+}
+.channels-add {
+  width: $buttonWidth;
+  button {
+    margin: 20%;
+    height: 30%;
+    border-radius: 5px;
+    outline: none;
+    border: 1px solid $myLove;
+    background-color:rgba($myLove,0.2);
+    &:focus{
+      background-color: #fff;
+    }
+    &:hover{
+      background-color:rgba($myLove,0.5);
+    }
+    &:active  {
+      background-color: $myLove;
+    }
+    &:disabled{
+      cursor: not-allowed;
+       background-color:$aDark;
+    }
+  }
+}
 #server {
   margin: auto;
-  width: 80%;
-  height: 10vh;
+  height: $height;
+  width: $serverWidth;
   position: relative;
   &.Online {
-    background-color: rgba(160, 160, 160, 0.3);
+    background-color: $aDark;
     #channels {
       *.not {
         background-color: transparent;
@@ -134,9 +255,9 @@ $more: rgba(0, 0, 0, 0.5);
     }
   }
   &.Offline {
-    background-color: rgba(160, 160, 160, 0.8);
+    background-color: $dDark;
     #channels {
-       *.exist {
+      *.exist {
         background-color: $dcolor;
         .chan-btn {
           &:hover {
@@ -150,13 +271,14 @@ $more: rgba(0, 0, 0, 0.5);
     justify-content: space-between;
     display: flex;
     flex-direction: row;
+    width: 100%;
     *:focus {
-        outline: none;
-      }
+      outline: none;
+    }
     > * {
       width: 8%;
       margin: auto;
-      height: 9vh;
+      height: $height;
     }
 
     div > span {
@@ -164,12 +286,12 @@ $more: rgba(0, 0, 0, 0.5);
       height: 100%;
     }
     *.chan-btn {
-      border-radius: 20%;
+      border-radius: 20px;
       width: 100%;
       height: 100%;
     }
     *.exist {
-      border-radius: 20%;
+      border-radius: 20px;
     }
   }
 }
--- src/pages/get_ss.vue
+++ src/pages/get_ss.vue
@@ -1,49 +1,59 @@
 <template>
-    <div class="flexColBox">
+    <div class="flexColBox servers">
         <ul v-for="s of servers" :id="s.host">
-            <single-ss :server="s"/>
+            <single-ss :host="s.host"/>
         </ul>
     </div>
 </template>
 
-<script>
-import SingleSS from '@/components/single_ss'
-import Global from '@/components/global'
-export default {
-    data(){
-        return{
-            servers:[]
-        }
-    },
-    components:{
-        Global,
-        "single-ss":SingleSS
-    },
-    methods:{
-        fetchData:function(){
-            let address=Global.ss1+'/admin/server/query'
-            console.log(address)
-            this.$http.get(
-                address,{
-                    params:{
-                        channels:1
-                    }
-                }
-            ).then(
-                res=>{
-                    if(res.body.code==1000){
-                        this.servers=res.body.data.list
-                        // console.log(this.servers)
-                    }
-                },
-                res=>{
-
-                }
-            )
-        }
-    },
-    mounted(){
-        this.fetchData()
-    }
+<style lang="scss" scoped>
+.servers {
+  // justify-content: flex-start;
+  width: 400px;
 }
+</style>
+
+
+<script>
+import SingleSS from "@/components/single_ss";
+import Global from "@/components/global";
+export default {
+  data() {
+    return {
+      servers: []
+    };
+  },
+  components: {
+    Global,
+    "single-ss": SingleSS
+  },
+  methods: {
+    fetchData: function() {
+      let address = Global.ss1 + "/admin/server/query";
+      console.log(address);
+      this.$http
+        .get(address, {
+          params: {
+            // channels:1
+          }
+        })
+        .then(
+          res => {
+            if (res.body.code == 1000) {
+              this.servers = res.body.data.list;
+              // console.log(this.servers)
+            } else {
+              this.$noty.error(res.body.msg);
+            }
+          },
+          res => {
+            this.$noty.error(res.statusText);
+          }
+        );
+    }
+  },
+  mounted() {
+    this.fetchData();
+  }
+};
 </script>
