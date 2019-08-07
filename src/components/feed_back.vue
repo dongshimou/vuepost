6ab82616a2f52038518e7e356cfce2a5b0fa9ef8
skadi <dongshimou@gmail.com>
2019-08-07 13:19:15 +0800

    add feedback todo

---
+++ src/components/feed_back.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <single-input></single-input>
+  </div>
+</template>
+
+<script>
+import SingleInput from "@/components/single_input";
+
+export default {
+  components: {
+    "single-input": SingleInput
+  }
+};
+</script>
