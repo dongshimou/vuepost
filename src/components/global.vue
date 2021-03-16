e0a5fca355267260b71e9047360bc63e144de380
shimou <dongshimou@gmail.com>
2021-03-16 22:43:47 +0800

    Update global.vue
    change hostname

--- src/components/global.vue
+++ src/components/global.vue
@@ -1,12 +1,12 @@
 <script>
 // [product]
-const host="https://fuckneusoft.com";
-const inter="https://interface.fuckneusoft.com/v1";
+const host="https://yanthems.com";
+const inter="https://interface.yanthems.com/v1";
 // [debug]
 // const host="http://localhost";
 // const inter="//localhost:12345/v1";
 
-const ss1="https://ss3.fuckneusoft.com:9998"+"/v1";
+const ss1="https://ss3.yanthems.com:9998"+"/v1";
 
 
 var isLogin=false;
